062adec5a4e57e17c17ef8b537dd85e8ae9d8a5a
Tomas Mac Carthy <i314082@hybris.local>
2017-04-07 12:10:08 +0100

    Initial commit

---
+++ src/main/java/com/hybris/yaas_universal_size_converter/yaas_universal_size_converter/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Created with [y] YaaS Service SDK version 4.11.2.
+ */
+/**
+ * Contains the application config.
+ */
+package com.hybris.yaas_universal_size_converter.yaas_universal_size_converter;
